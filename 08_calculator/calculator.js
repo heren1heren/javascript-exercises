10d3c7b36907dadfcb0801c03da4609243d6462c
heren <heren1heren@gmail.com>
2023-08-15 06:04:42 -0600

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(arr) {
+	// sum of the array
+  let sum = 0; 
+ for (let i = 0; i <= arr.length -1; i++) {
+    sum += arr[i];
+ }
+  return sum;
+};
+
+const multiply = function(...num) {
+  // don't know how much nums we have so can't use for..i loop
+  let result = 1;
+  for ( let  n of num) {
+    result *= n;
+  }
+        
+  return result;
+
+};
+
+const power = function(base, exponent) {
+  let result = 1;
+  for (i =1 ; i<= exponent; i++) {
+    result *= base;
+  }  
+ return result;
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+  for( i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
