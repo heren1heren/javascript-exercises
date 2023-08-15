f72e3d18ba497be95cf285182d9ba6e9a920eb41
heren <heren1heren@gmail.com>
2023-08-15 04:33:44 -0600

    uoeueo

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(FDegree) {
+    return CDegree =parseFloat(((FDegree - 32) * 5/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(CDegree) {
+
+    return FDegree = parseFloat(((CDegree *9/5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
